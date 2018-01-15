00f61638d5a42c056acbf264887182d2c081dedb
Alex de Miranda Moreira <alex.t0565.beyond@xpi.com.br>
2018-01-15 20:07:21 -0200

    Exmplo de como carregar chunk com import.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
+
+import(/* webpackChunkName: "lodash" */ 'lodash').then(_ => {
+    console.log('ok')
+  }).catch(error => 'error');
+
+
 ReactDOM.render(React.createElement(App), document.getElementById('app'));
